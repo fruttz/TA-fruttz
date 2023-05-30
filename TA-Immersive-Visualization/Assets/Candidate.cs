4c9ec77d89429b9523e85a14e721205b57de4a49
fruttz <dito.arjwasa@gmail.com>
2023-05-31 02:53:50 +0700

    added text for bar

--- TA-Immersive-Visualization/Assets/Candidate.cs
+++ TA-Immersive-Visualization/Assets/Candidate.cs
@@ -42,7 +42,86 @@ public class Candidate
         this.name = candidate_name;
     }
 
-    public int GetTotalValue(){
+    public int getProvince(int province){
+        switch(province){
+            case 0:
+                return this.getTotalValue();
+            case 1:
+                return this.aceh;
+            case 2:
+                return this.bali;
+            case 3:
+                return this.banten;
+            case 4:
+                return this.bengkulu;
+            case 5:
+                return this.dki_jakarta;
+            case 6:
+                return this.gorontalo;
+            case 7:
+                return this.jambi;
+            case 8:
+                return this.jawa_barat;
+            case 9:
+                return this.jawa_tengah;
+            case 10:
+                return this.jawa_timur;
+            case 11:
+                return this.kalimantan_barat;
+            case 12:
+                return this.kalimantan_selatan;
+            case 13:
+                return this.kalimantan_tengah;
+            case 14:
+                return this.kalimantan_timur;
+            case 15:
+                return this.kalimantan_utara;
+            case 16:
+                return this.kepulauan_bangka_belitung;
+            case 17:
+                return this.kepulauan_riau;
+            case 18:
+                return this.kepulauan_riau;
+            case 19:
+                return this.lampung;
+            case 20:
+                return this.luar_negeri;
+            case 21:
+                return this.maluku;
+            case 22:
+                return this.maluku_utara;
+            case 23:
+                return this.ntb;
+            case 24:
+                return this.ntt;
+            case 25:
+                return this.papua;
+            case 26:
+                return this.papua_barat;
+            case 27:
+                return this.riau;
+            case 28:
+                return this.sulawesi_barat;
+            case 29:
+                return this.sulawesi_selatan;
+            case 30:
+                return this.sulawesi_tengah;
+            case 31:
+                return this.sulawesi_utara;
+            case 32:
+                return this.sumatera_barat;
+            case 33:
+                return this.sumatera_selatan;
+            case 34:
+                return this.sumatera_utara;
+            case 35:
+                return this.yogyakarta;
+            default:
+                return this.getTotalValue();
+        }
+    }
+
+    public int getTotalValue(){
         int total = 0;
         total += this.aceh;
         total += this.bali;
--- TA-Immersive-Visualization/Assets/VisualizeBar.cs
+++ TA-Immersive-Visualization/Assets/VisualizeBar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Proyecto26;
+using TMPro;
 using UnityEngine;
 using UnityEditor;
 
@@ -9,6 +10,7 @@ public class VisualizeBar : MonoBehaviour
     public string db_url = "https://immersive-visualization-pemilu-default-rtdb.asia-southeast1.firebasedatabase.app/";
     public string candidateName;
     enum Province {
+        total,
         aceh, 
         bali, 
         banten, 
@@ -44,11 +46,11 @@ public class VisualizeBar : MonoBehaviour
         sumatera_selatan,
         sumatera_utara,
         yogyakarta,
-        total
     }
     [SerializeField] 
     private Province province = new Province();
     public GameObject Bar;
+    public TextMeshPro voterValue;
     private float barWidth = 0.5f;
     private float updateInterval = 0.001f;
     private float nextUpdateTime = 10f;
@@ -62,121 +64,17 @@ public class VisualizeBar : MonoBehaviour
         Candidate candidate = new Candidate(candidateName);
         RestClient.Get<Candidate>(db_url + candidate.name + ".json").Then(response => {
             candidate = response;
-            Province child = Province.total;
-            switch(child){
-                case Province.aceh:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.aceh), barWidth);
-                    break;
-                case Province.bali:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.bali), barWidth);
-                    break;
-                case Province.banten:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.banten), barWidth);
-                    break;
-                case Province.bengkulu:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.bengkulu), barWidth);
-                    break;
-                case Province.dki_jakarta:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.dki_jakarta), barWidth);
-                    break;
-                case Province.gorontalo:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.gorontalo), barWidth);
-                    break;
-                case Province.jambi:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.jambi), barWidth);
-                    break;
-                case Province.jawa_barat:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.jawa_barat), barWidth);
-                    break;
-                case Province.jawa_tengah:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.jawa_tengah), barWidth);
-                    break;
-                case Province.jawa_timur:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.jawa_timur), barWidth);
-                    break;
-                case Province.kalimantan_barat:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.kalimantan_barat), barWidth);
-                    break;
-                case Province.kalimantan_selatan:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.kalimantan_selatan), barWidth);
-                    break;
-                case Province.kalimantan_tengah:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.kalimantan_tengah), barWidth);
-                    break;
-                case Province.kalimantan_timur:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.kalimantan_timur), barWidth);
-                    break;
-                case Province.kalimantan_utara:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.kalimantan_utara), barWidth);
-                    break;
-                case Province.kepulauan_bangka_belitung:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.kepulauan_bangka_belitung), barWidth);
-                    break;
-                case Province.kepulauan_riau:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.kepulauan_riau), barWidth);
-                    break;
-                case Province.lampung:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.lampung), barWidth);
-                    break;
-                case Province.luar_negeri:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.luar_negeri), barWidth);
-                    break;
-                case Province.maluku:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.maluku), barWidth);
-                    break;
-                case Province.maluku_utara:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.maluku_utara), barWidth);
-                    break;
-                case Province.ntb:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.ntb), barWidth);
-                    break;
-                case Province.ntt:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.ntt), barWidth);
-                    break;
-                case Province.papua:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.papua), barWidth);
-                    break;
-                case Province.papua_barat:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.papua_barat), barWidth);
-                    break;
-                case Province.riau:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.riau), barWidth);
-                    break;
-                case Province.sulawesi_barat:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.sulawesi_barat), barWidth);
-                    break;
-                case Province.sulawesi_selatan:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.sulawesi_selatan), barWidth);
-                    break;
-                case Province.sulawesi_tengah:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.sulawesi_tengah), barWidth);
-                    break;
-                case Province.sulawesi_tenggara:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.sulawesi_tenggara), barWidth);
-                    break;
-                case Province.sulawesi_utara:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.sulawesi_utara), barWidth);
-                    break;
-                case Province.sumatera_barat:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.sumatera_barat), barWidth);
-                    break;
-                case Province.sumatera_selatan:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.sumatera_selatan), barWidth);
-                    break;
-                case Province.sumatera_utara:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.sumatera_utara), barWidth);
-                    break;
-                case Province.yogyakarta:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.yogyakarta), barWidth);
-                    break;
-                case Province.total:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.GetTotalValue()), barWidth);
-                    break;
-                default:
-                    Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.GetTotalValue()), barWidth);
-                    break;
-            }
+            adjustBar(candidate);
+            setTextValue(candidate.getProvince((int)province));
         });
+    }
+
+    private void adjustBar(Candidate candidate){
+        Bar.transform.localScale = new Vector3(barWidth, normalizeValue(candidate.getProvince((int)province)), barWidth);
+    }
+
+    private void setTextValue(int value){
+        voterValue.text = value.ToString();
     }
 
     void Start()
